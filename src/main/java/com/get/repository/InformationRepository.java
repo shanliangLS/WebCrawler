3fbd884123a90d3fa0cb4eddef01b3d286ae6219
skyefee <feng45k@qq.com>
2019-12-29 18:40:04 +0800

    添加展示信息

--- src/main/java/com/get/repository/InformationRepository.java
+++ src/main/java/com/get/repository/InformationRepository.java
@@ -19,6 +19,28 @@ public interface InformationRepository extends JpaRepository<Information,Long> {
 
     List<Information> findAllByTaskId(Long taskId);
 
+    /**
+     * 返回该用户除去内容的所有信息
+     * @param taskId
+     * @return
+     */
+    @Query(value = "select * from information",nativeQuery = true)
+    List<Information> findAllExcludeContentByTaskId(Long taskId);
+
+    /**
+     * 根据class查找表classes中的name
+     *
+     */
+    @Query(value = "select name from classes where id=?1",nativeQuery = true)
+    String selectClassesNameById(Long id);
+
+    /**
+     * 返回信息的具体内容，id，title，content，author，url，picture
+     *
+     */
+    @Query(value = "select * from information where id=?1",nativeQuery = true)
+    Information selectInformationDetailById(Long id);
+
     //    @Transactional
 //    @Query("select id,title,createTime,author,content,picture,near,keyWords,classes from Information where classes=:classes")
 //    List<Information> getByClasses(@Param("classes") String classes);
